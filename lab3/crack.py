34b8f7905e865c6c9e4aec374da99e3651368d14
Anastasia Shvetsova <sweet.santonica@gmail.com>
2022-10-21 23:32:57 +0300

    Verification is without filename check now

--- lab3/crack.py
+++ lab3/crack.py
@@ -1,4 +1,21 @@
 import argparse
+from verifier import verify
 
 
-def main()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action='store_true', help="Output information of cracking process")
+    parser.add_argument("file", help="Path to file")
+    args = parser.parse_args()
+    v = args.verbose
+    pathfile = args.file
+    if not verify(pathfile):
+        raise Exception("File is not valid!")
+    if v:
+        print("Valid file!")
+    with open(pathfile, 'rb') as file:
+
+    if v:
+        print(f'Hash function: {hash_func}\nCipher algorythm: {cipher}\n')
+
+
--- lab3/verifier.py
+++ lab3/verifier.py
@@ -1,65 +1,31 @@
 import argparse
-from gen import KEY_SIZE
-
-hash_funcs = ['md5', 'sha1']
-ciphers = ['3des', 'aes128', 'aes192', 'aes256']
 
 
-def verify() -> bool:
-    parser = argparse.ArgumentParser(description="Verification for files encrypted by study cipher")
-    parser.add_argument("file", help="File with extension '.enc'")
-    args = parser.parse_args()
-    path = args.file
-    filename = path.split('\\')[-1]
-    unscored = filename.split('_')
-    hash_func = unscored[0]
-    cipher = unscored[1]
-    key = bytes.fromhex(unscored[2][:-4])
-    ext = unscored[2][-4:]
-    if hash_func not in hash_funcs:
-        print(hash_func)
+def verify(pathfile: str) -> bool:
+    ext = pathfile.split('.')[-1]
+    if ext != 'enc':
         return False
-    if cipher not in ciphers:
-        print(cipher)
-        return False
-    if len(key) != KEY_SIZE:
-        print(len(key))
-        return False
-    if ext != '.enc':
-        print(ext)
-        return False
-    with open(path, 'rb') as file:
+    with open(pathfile, 'rb') as file:
         sign = file.read(3).decode('ascii')
         if sign != 'ENC':
             print(sign)
             return False
         hash_byte = file.read(1)
-        if hash_byte == b'\x00':
-            if hash_func != 'md5':
-                return False
-        else:
-            if hash_byte != b'\x01':
-                return False
+        if hash_byte != b'\x00' and hash_byte != b'\x01':
+            return False
         cipher_byte = file.read(1)
-        match cipher_byte:
-            case b'\x00':
-                if cipher != '3des':
-                    return False
-            case b'\x01':
-                if cipher != 'aes128':
-                    return False
-            case b'\x02':
-                if cipher != 'aes192':
-                    return False
-            case _:
-                if cipher_byte != '\x03':
-                    return False
+        if cipher_byte != b'\x00' and cipher_byte != b'\x01' and cipher_byte != b'\x02' and cipher_byte != b'\x03':
+            return False
     return True
 
 
 if __name__ == '__main__':
     try:
-        print(verify())
+        parser = argparse.ArgumentParser(description="Verification for files encrypted by study cipher")
+        parser.add_argument("file", help="File with extension '.enc'")
+        args = parser.parse_args()
+        path = args.file
+        print(verify(path))
     except Exception as msg:
         print(msg)
         print(False)
