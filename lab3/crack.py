d9e94c1f6a6f3346dfa2149f2c65c6f5ed04ae1a
Anastasia Shvetsova <sweet.santonica@gmail.com>
2022-10-22 02:50:05 +0300

    Finished crack.py

--- lab3/crack.py
+++ lab3/crack.py
@@ -1,5 +1,24 @@
 import argparse
 from verifier import verify
+from gen import NONCE_SIZE, TDES_IV_SIZE, AES_IV_SIZE, KEY_SIZE
+import multiprocessing as mp
+import time
+
+
+def generate_candidates(a: tuple):
+    start_t = time.time()
+    for i in range(a[0], a[1]):
+        candidate = i.to_bytes(KEY_SIZE, 'big')
+        if candidate == a[2]:
+            if a[3]:
+                wasted_time = time.time() - start_t
+                speed = (i - a[0]) / wasted_time
+                print(f'Current: {hex(a[0])}-{hex(a[1])}, speed = {speed} c/s')
+            return candidate
+    if a[3]:
+        wasted_time = time.time() - start_t
+        speed = (a[1] - a[0]) / wasted_time
+        print(f'Current: {hex(a[0])}-{hex(a[1])}, speed = {speed} c/s')
 
 
 def main():
@@ -11,14 +30,50 @@ def main():
     pathfile = args.file
     if not verify(pathfile):
         raise Exception("File is not valid!")
-    if v:
-        print("Valid file!")
+    print("Valid file!")
     with open(pathfile, 'rb') as file:
         file.read(3)  # Pass ENC
         hash_byte = file.read(1)
         cipher_byte = file.read(1)
-
+        nonce = file.read(NONCE_SIZE)
+        if cipher_byte == b'\x00':
+            iv = file.read(TDES_IV_SIZE)
+        else:
+            iv = file.read(AES_IV_SIZE)
+        cipher_text = file.read()
+    if hash_byte == b'\x00':
+        hash_func = 'md5'
+    else:
+        hash_func = 'sha1'
+    cipher = ''
+    match cipher_byte:
+        case b'\x00':
+            cipher = '3des'
+        case b'\x01':
+            cipher = 'aes128'
+        case b'\x02':
+            cipher = 'aes192'
+        case b'\x03':
+            cipher = 'aes256'
     if v:
-        print(f'Hash function: {hash_func}\nCipher algorythm: {cipher}\n')
+        print(f'HMAC-{hash_func.upper()}, {cipher.upper()}')
+        print(f'NONCE: {nonce.hex()}')
+        print(f'IV: {iv.hex()}')
+        print(f'CT: {cipher_text.hex()}')
+
+    parole = bytes.fromhex(pathfile.split('_')[-1][:-4])
+    print("Cracking...")
+    with mp.Pool(mp.cpu_count() - 1) as p:
+        step = 2 ** 32 // (mp.cpu_count() * 4)
+        ranges = [(a, b, parole, v) for a, b in zip(range(0, 2**32 - step, step), range(step, 2 ** 32, step))]
+        start = time.time()
+        res = p.map(generate_candidates, ranges)
+        t = time.time() - start
+        speed = 2**32 / t
+        for key in res:
+            if key is not None:
+                print(f'Found: {key.hex()}, average speed = {speed} c/s')
 
 
+if __name__ == '__main__':
+    main()
